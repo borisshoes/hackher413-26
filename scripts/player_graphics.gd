264b52c7f30d5c6f9191ec579e072d846efbeed6
Snivyxxy <snivyxxy@gmail.com>
2026-02-21 17:30:55 -0500

    Completed Player Animation System

--- scripts/player_graphics.gd
+++ scripts/player_graphics.gd
@@ -8,7 +8,7 @@ extends Node3D
 
 enum BodyPart {EARS = 0, FACE = 1, BODYARMS = 2, LEG = 3}
 
-enum CardDirection {NORTH = '0', SOUTH = '0', EAST = '0', WEST = '0'}
+enum CardDirection {SOUTH = 0, WEST = 1, NORTH = 2, EAST = 3}
 enum PlayerAction {IDLE, WALK}
 
 
@@ -16,26 +16,23 @@ enum PlayerAction {IDLE, WALK}
 @export var state = PlayerAction.IDLE
 @export var carrying = false
 
-var lastFacing = facing
-var lastCarrying = carrying
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var tempDir = facing
+	if tempDir == CardDirection.EAST:
+		tempDir = CardDirection.WEST
 	
-	if facing == CardDirection.EAST:
-		for s in sprites:
-			s.flip_h = true
-	else:
-		for s in sprites:
-			s.flip_h = false
-			
-			
-	match state:
-		PlayerAction.IDLE:
-			
-			if carrying:
-				sprites[BodyPart.BODYARMS].play()
-			
-		PlayerAction.WALK:
-			pass
-			
+	var dirstr = str(tempDir)
+	
+	for s in sprites:
+		s.flip_h = (facing == CardDirection.EAST)
+		
+		match state:
+			PlayerAction.IDLE:
+				if s.animation != "Idle_" + str(dirstr):
+					s.play("Idle_" + dirstr)
+			PlayerAction.WALK:
+				if s.animation != "Walk_" + dirstr:
+					s.play("Walk_" + dirstr)
+	if carrying:
+		sprites[BodyPart.BODYARMS].play("Hold_" + dirstr)
