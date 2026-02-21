0e82d161b1105298ff29d9f84f9928f8dfd82dd3
Eli Grassi <23egrassi@gmail.com>
2026-02-21 18:53:44 -0500

    changed the player and item, added a test item to map and made map official

--- scripts/player_graphics.gd
+++ scripts/player_graphics.gd
@@ -2,7 +2,7 @@ class_name PlayerGraphics
 extends Node3D
 
 
-
+@onready var player_body = $".."
 
 @export var sprites: Array[AnimatedSprite3D]
 
@@ -12,12 +12,34 @@ enum CardDirection {SOUTH = 0, WEST = 1, NORTH = 2, EAST = 3}
 enum PlayerAction {IDLE, WALK}
 
 
-@export var facing = CardDirection.NORTH
-@export var state = PlayerAction.IDLE
-@export var carrying = false
+@export var facing := CardDirection.NORTH
+@export var state := PlayerAction.IDLE
+@export var carrying := false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if player_body.velocity.length() > 0:
+		state = PlayerAction.WALK
+	else:
+		state = PlayerAction.IDLE
+	
+	if player_body.holding_something != null:
+		carrying = true
+	else:
+		carrying = false
+	
+	if player_body.velocity.x < 0:
+		facing = CardDirection.WEST
+	elif player_body.velocity.x > 0:
+		facing = CardDirection.EAST
+	
+	if player_body.velocity.z < 0:
+		facing = CardDirection.NORTH
+	elif player_body.velocity.z > 0:
+		facing = CardDirection.SOUTH
+		
+	
 	var tempDir = facing
 	if tempDir == CardDirection.EAST:
 		tempDir = CardDirection.WEST
