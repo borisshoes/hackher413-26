276a7e06dd768196604a2cda47442effbd74bfdc
Snivyxxy <snivyxxy@gmail.com>
2026-02-22 09:15:53 -0500

    Potion Silhouettes

---
+++ scripts/PotionSilhouette.gd
@@ -0,0 +1,25 @@
+extends Node
+
+const indexconversion = [0,0,2,1,0,4,3]
+
+@export var potionID:int
+
+@export var potionsprites:Array[Sprite2D]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for i in potionsprites:
+		if(indexconversion[potionID] == 4):
+			if i.name == "Out":
+				i.region_rect.position.x = 48
+				i.region_rect.position.y = 32
+			else:
+				i.region_rect.position.x = 32
+				i.region_rect.position.y = 32
+		else:
+			i.region_rect.position.x = indexconversion[potionID] * 16
+			i.region_rect.position.y = 16
+		
+			
+	
--- scripts/poster-interactable.gd
+++ scripts/poster-interactable.gd
@@ -6,10 +6,10 @@ extends Workstation
 # Called when the server registers the player starting to use this workstation
 func start_use(peer_id:int):
 	print("[CashRegister] start_use() for peer: ", peer_id)
-	print("[CashRegister] Player is now actively using the cauldron.")
+	print("[CashRegister] Player is now actively using the poster.")
 	pop_up.visible = true
 # Called when the server registers the player ending use
 func end_use(peer_id:int):
 	print("[CashRegister] end_use() for peer: ", peer_id)
-	print("[CashRegister] Player has stopped using the cauldron.")
+	print("[CashRegister] Player has stopped using the poster.")
 	pop_up.visible = false
